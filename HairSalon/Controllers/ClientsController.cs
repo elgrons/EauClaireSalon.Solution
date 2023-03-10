8a74015aa95512798fc413383bb133ae0ab75c6e
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-10 11:40:23 -0800

    update Create, Details, and Index in Views.Clients

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -25,7 +25,7 @@ namespace HairSalon.Controllers
 
     public ActionResult Create()
     {
-      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Client Name");
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "StylistName");
       return View();
     }
 
