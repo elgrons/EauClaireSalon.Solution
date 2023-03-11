b8bf27b8bd16fc0d646bccd8b5cc947bac6237cc
Eliot Gronstal <eliot.lauren@gmail.com>
2023-03-10 16:48:21 -0800

    update Tests with 'not required' wording.

--- HairSalon.Tests/ModelTests/StylistTests.cs
+++ HairSalon.Tests/ModelTests/StylistTests.cs
@@ -1,22 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName.Models;
+using HairSalon.Models;
 using System.Collections.Generic; 
 using System;
 
-namespace ProjectName.Tests
+namespace HairSalon.Tests
 {
   [TestClass]
-  public class ClassNameTests
+  public class StylistTests
   {
   [TestMethod]
-    public void ClassNameConstructor_CreatesInstanceOfClassName_ClassName()
-    {
-      // Arrange
-      ClassName newClass = new ClassName(2, 3, 8);
-      // Act
-      //not needed here
-      // Assert
-    Assert.AreEqual(typeof(ClassName), newClass.GetType());
+    public void StylistConstructor_CreatesInstanceOfStylist_Stylist()
+    { 
+    //NoTestsRequiredThisWeek   
     }
   }
 }
